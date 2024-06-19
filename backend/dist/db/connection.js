8181a442c77be7b1b0745ce351bc46a78835d144
Sugata <sarkarsugata57@gmail.com>
2024-06-19 13:00:11 +0530

    setting up the folders

---
+++ backend/dist/db/connection.js
@@ -0,0 +1,21 @@
+import { connect, disconnect } from 'mongoose';
+async function connectToDB() {
+    try {
+        await connect(process.env.MONGODB_URL);
+        console.log('Connected to database');
+    }
+    catch (error) {
+        console.error('Error connecting to database: ', error);
+    }
+}
+async function disconnectFromDB() {
+    try {
+        await disconnect();
+        console.log('Disconnected from database');
+    }
+    catch (error) {
+        console.error('Error disconnecting from database: ', error);
+    }
+}
+export { connectToDB, disconnectFromDB };
+//# sourceMappingURL=connection.js.map
---
+++ backend/src/index.ts
@@ -0,0 +1,13 @@
+import app from './app.js';
+import { connectToDB } from './db/connection.js';
+
+//connection and listener
+const PORT = process.env.PORT || 5000;
+connectToDB().then(() => {
+  app.listen(PORT, () => {
+    console.log('Server is running on port 5000');
+  });  
+})
+.catch((error) => {
+  console.error('Error connecting to database: ', error);
+});
