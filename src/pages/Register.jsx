87926f2f9a39cc2065e27099baa3707b558b5e54
Sugata <sarkarsugata57@gmail.com>
2024-12-27 00:48:25 +0530

    booting up

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -10,18 +10,19 @@ function Register() {
     return (
         <>
         <PageTitle title="Create an account" />
-        <div className="">
-            <div className="">
-                <Link>
-                <img src={logoLight} alt="Pandora logo" width={133} height={24} />
-                <img src={logoDark} alt="Pandora logo" width={133} height={24} />
+        <div className="relative w-screen h-dvh p-2 grid grid-cols-1 lg:grid-cols-[1fr, 1.2fr] lg:gap-2">
+            <div className="flex flex-col p-4">
+                <Link to="/" className='max-w-max mx-auto lg:mx-0'>
+                <img src={logoLight} alt="Pandora logo" width={133} height={24} className='dark:hidden' />
+                <img src={logoDark} alt="Pandora logo" width={133} height={24} className='hidden dark:block' />
                 </Link> 
-                <div className="">
-                    <h2>Create an account</h2>
-                    <p>Register today and gain access to powerful tools that will supercharge your idea</p>
+                <div className="flex flex-col gap-2 max-w-[480px] w-full mx-auto">
+                    <h2 className='text-displaySmall font-semibold text-light-onBackground
+                    dark:text-dark-onBackground text-center'>Create an account</h2>
+                    <p className='text-bodyLarge text-light-onSurfaceVariant dark:text-dark-onSurfaceVariant text-center mt-1 mb-5'>Register today and gain access to powerful tools that will supercharge your idea</p>
                     <Form 
                     method='POST'
-                    className=''>
+                    className='grid grid-cols-1 gap-4'>
                         <TextFields
                         name='name'
                         label="Full Name"
@@ -55,7 +56,8 @@ function Register() {
                 <p className=''>&copy; {new Date().getFullYear()} Pandora. All rights reserved</p>
             </div>
             <div className="">
-                <img src={banner} alt="Pandora logo" width={133} height={24} />
+                <img src={banner} alt="Pandora logo" className='img-cover'  />
+                <p className=''>Pandora - chat to supercharge your idea</p>
             </div>
         </div>
         </>
